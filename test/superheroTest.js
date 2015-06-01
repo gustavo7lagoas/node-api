e5274e5e922059c16d0c1a59101e57e4b837e513
gustavo7lagoas <gustavo7lagoas@gmail.com>
2015-06-01 11:26:36 -0300

    tests for main http requests

--- test/superheroTest.js
+++ test/superheroTest.js
@@ -43,6 +43,86 @@ describe('Superhero API',function() {
             });
         });
     });
+    describe('Creates superheroes', function() {
+        beforeEach(function(done) {
+            Superhero.remove().exec(function(res) { done(); });
+        });
+        it('create one superheroes', function(done) {
+            request
+                .post('/api/superheroes')
+                .set('Accept', 'application/x-www-form-urlencoded')
+                .send({
+                    'name' : 'batman',
+                    'power' : 'money',
+                    'age' : 25
+                })
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) throw err;
+                    res.body.should.be.deep.equal( { message : 'Superhero created!'} );
+                    done();
+                });
+        });
+    });
+    describe('Get specific superhero', function() {
+        beforeEach(function(done) {
+            Superhero.remove().exec(function(res) { done(); });
+        });
+        it('get a specific superhero', function(done) {
+            factory.create('superhero', function(err, superhero) {
+                var superhero_id = superhero.id;
+                request
+                    .get('/api/superheroes/' + superhero_id)
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end(function(err, res) {
+                        if (err) throw err;
+                        res.body.name.should.be.equal(superhero.name);
+                        done();
+                    });
+            });
+        });
+    });
+    describe('Updates a specific superhero', function() {
+        beforeEach(function(done) {
+            Superhero.remove().exec(function(res) { done(); });
+        });
+        it('updates', function(done) {
+            factory.create('superhero', function(err, superhero) {
+                var updatedSuperHero = superhero;
+                updatedSuperHero.name = 'batman';
+                request
+                    .put('/api/superheroes/' + superhero.id)
+                    .set('Accept', 'application/x-www-form-urlencoded')
+                    .send(updatedSuperHero)
+                    .expect(200)
+                    .end(function(err, res) {
+                        res.body.should.be.deep.equal({ message: 'Superhero updated!' });
+                        done();
+                    });
+            });
+        });
+    });
+    describe('Deletes a superhero', function() {
+        beforeEach(function(done) {
+            Superhero.remove().exec(function(res) { done(); });
+        });
+        it('deletes', function(done) {
+            factory.create('superhero', function(err, superhero) {
+                request
+                    .delete('/api/superheroes/' + superhero.id)
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end(function(err, res) {
+                        res.body.should.be.deep.equal({ message: 'Successfully deleted' });
+                        done();
+                    });
+            });
+        });
+    });
 });
 
 
